5648ad6b395c65e6b5d4e08b2dfa1eb8a1f6a72c
lumabreakout <superpasswort99>
2012-10-12 16:56:23 +0200

    tests added

--- src/data/PlayGrid.java
+++ src/data/PlayGrid.java
@@ -1,6 +1,7 @@
 package data;
 
 
+import java.util.LinkedList;
 import java.util.List;
 
 import data.bricks.AbstractBrick;
@@ -17,8 +18,56 @@ public class PlayGrid {
 	
 	private Slider slider;
 	
-	public PlayGrid() {
-		
+	public PlayGrid(int height, int width) {
+		super();
+		setHeight(height);
+		setWidth(width);
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public List<Ball> getBalls() {
+		if (balls == null) {
+			balls = new LinkedList<Ball>();			
+		}		
+		return balls;
+	}
+
+	public void addBall(Ball ball) {
+		getBalls().add(ball);		
+	}
+
+	public List<AbstractBrick> getBricks() {
+		if (bricks == null) {
+			bricks = new LinkedList<AbstractBrick>();
+		}
+		return bricks;
+	}
+
+	public void addBrick(AbstractBrick brick) {
+		getBricks().add(brick);
+	}
+
+	public Slider getSlider() {
+		return slider;
+	}
+
+	public void setSlider(Slider slider) {
+		this.slider = slider;
 	}
 	
 
